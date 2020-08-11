653eb91747e019afa7e1bf425a4d3c86ec2d1289
Tricia Blosser <Tricia.blosser@gmail.com>
2020-08-11 12:13:02 -0400

    finished steps 1-4

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+require 'securerandom'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+    @word = params[:word]
+  end
+end
